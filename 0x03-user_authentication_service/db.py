f3ba7bb059bc24fbebf1a789696fa75c1ef9771b
opxzy22 <aopeyem84@gmail.com>
2024-02-22 22:06:10 +0100

    done

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -1,11 +1,11 @@
+#!/usr/bin/env python3
 """DB module
 """
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.orm.exc import NoResultFound
 from sqlalchemy.orm.session import Session
-from sqlalchemy.exc import NoResultFound, InvalidRequestError
-
 
 from user import Base, User
 
@@ -17,7 +17,7 @@ class DB:
     def __init__(self) -> None:
         """Initialize a new DB instance
         """
-        self._engine = create_engine("sqlite:///a.db", echo=True)
+        self._engine = create_engine("sqlite:///a.db", echo=False)
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
@@ -31,45 +31,44 @@ class DB:
             self.__session = DBSession()
         return self.__session
 
-    def add_user(self, email, hashed_password):
-        user = User(email=email, hashed_password=hashed_password)
-        self._session.add(user)
-        self._session.commit()
-
-        return user
-
-    def find_user_by(self, **kwargs):
-        """a method that query the database
-            Kwargs: keyword argument to be queried in the DB
-            raise NoResultFound error if no user found
-            raise InvalidRequestError if invalid query request
-            returns user if found
-        """
-        user = self._session.query(User).filter_by(**kwargs).first()
+    def add_user(self, email: str, hashed_password: str) -> User:
+        """ creates a user and returns it """
         try:
-            if user is None:
-                raise NoResultFound('no result found')
-        except InvalidRequestError:
-            raise InvalidRequestError('invalid')
-
+            user = User(email=email, hashed_password=hashed_password)
+            self._session.add(user)
+            self._session.commit()
+        except Exception:
+            self._session.rollback()
+            user = None
         return user
+
+    def find_user_by(self, **kwargs) -> User:
+        """ finds a user by an arbitary keyword argument """
+        if not kwargs:
+            raise InvalidRequestError
+        valid_attrs = ['id', 'email',
+                       'hashed_password',
+                       'session_id', 'reset_token']
+        for key in kwargs.keys():
+            if key not in valid_attrs:
+                raise InvalidRequestError
+        try:
+            return self._session.query(User).filter_by(**kwargs).one()
+        except Exception:
+            raise NoResultFound
 
     def update_user(self, user_id: int, **kwargs) -> None:
-        """
-            a method that update a user
-            finds the user by id with the find_user_by method
-            loops through the key word arg to be update
-            updates and commit
-            raise value error if arg does not correspond with
-            user attribute
-        """
-        try:
-            user = self.find_user_by(id=user_id)
-
-            for key, value in kwargs.items():
-                if hasattr(user, key):
-                    setattr(user, key, value)
-            self._session.commit()
-
-        except ValueError:
-            raise ValueError('Error')
+        """ updates a user """
+        if not kwargs:
+            raise ValueError
+        user = self.find_user_by(id=user_id)
+        if user:
+            valid_attrs = ['id', 'email',
+                           'hashed_password',
+                           'session_id', 'reset_token']
+            for key, val in kwargs.items():
+                if key not in valid_attrs:
+                    raise ValueError
+                setattr(user, key, val)
+        self._session.add(user)
+        self._session.commit()
--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -1,15 +1,19 @@
-from sqlalchemy import Column, Integer, String
+#!/usr/bin/env python3
+""" This module contains a user class """
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import String, Integer, Column
+
 
 Base = declarative_base()
 
 
 class User(Base):
-    """ a User model for the user database table"""
-    __tablename__ = 'user'
+    """ A user class mapped to a db table """
+
+    __tablename__ = 'users'
 
     id = Column(Integer, primary_key=True)
-    email = Column(String, nullable=False)
-    hashed_password = Column(String, nullable=False)
-    session_id = Column(String, nullable=True)
-    reset_token = Column(String, nullable=True)
+    email = Column(String(250), nullable=False)
+    hashed_password = Column(String(250), nullable=False)
+    session_id = Column(String(250), nullable=True)
+    reset_token = Column(String(250), nullable=True)

13a37fea7d6da6b15621436c8eb225e4c3265624
opxzy22 <aopeyem84@gmail.com>
2024-02-24 08:55:15 +0000

    done

--- 0x03-user_authentication_service/main.py
+++ 0x03-user_authentication_service/main.py
@@ -1,8 +1,15 @@
-
 #!/usr/bin/env python3
 """
 Main file
 """
-from auth import _hash_password
 
-print(_hash_password("Hello Holberton"))
+from db import DB
+from user import User
+
+my_db = DB()
+
+user_1 = my_db.add_user("test@test.com", "SuperHashedPwd")
+print(user_1.id)
+
+user_2 = my_db.add_user("test1@test.com", "SuperHashedPwd1")
+print(user_2.id)
